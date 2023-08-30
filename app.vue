772f004dc3df7c332ec55f11e17bb30c31cb4c22
kakelay <kakelay18052002@gmail.com>
2023-08-31 01:41:34 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,347 @@
+<template>
+  <div class="bg-gray-100 min-h-screen">
+    <!-- Header section -->
+    <header class="bg-blue-500 p-4 text-white">
+      <div class="container mx-auto">
+        <div class="flex items-center">
+          <img
+            src="https://scontent.fpnh5-5.fna.fbcdn.net/v/t39.30808-6/370567086_703693364913606_2397925962208215234_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a2f6c7&_nc_ohc=R02srbZtjPgAX_U0RhC&_nc_ht=scontent.fpnh5-5.fna&oh=00_AfDUKiIfgPN4qjMhwa2kDKiXEKwf0317maBq5dfdV-yy5Q&oe=64F49B61"
+            alt="Profile Image" class="w-32 h-32 rounded-full">
+          <div class="ml-4">
+            <h1 class="text-3xl font-bold">
+              KakElay
+            </h1>
+            <p class="text-lg mt-2 font-bold">Flutter | Java | Spring Boot | Nuxt.js</p>
+          </div>
+        </div>
+      </div>
+    </header>
+
+
+    <section class="bg-white py-10">
+      <div class="container mx-auto">
+        <h2 class="text-2xl font-bold mb-4 text-center">My Profile</h2>
+        <div class="flex justify-center">
+          <img
+            src="https://scontent.fpnh5-5.fna.fbcdn.net/v/t39.30808-6/370567086_703693364913606_2397925962208215234_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a2f6c7&_nc_ohc=R02srbZtjPgAX_U0RhC&_nc_ht=scontent.fpnh5-5.fna&oh=00_AfDUKiIfgPN4qjMhwa2kDKiXEKwf0317maBq5dfdV-yy5Q&oe=64F49B61"
+            alt="Profile Image" class="w-32 h-32 rounded-full">
+        </div>
+        <div class="text-center mt-4">
+          <h3 class="text-xl font-semibold">Kak Elay</h3>
+          <p class="text-gray-700">Flutter | Java | Spring Boot | Nuxt.js</p>
+          <p class="text-gray-700">"It's going to be hard, but hard doesn't mean impossible."</p>
+
+          <div class="flex justify-center mt-4">
+            <div class="mx-2 flex flex-col items-center">
+              <a href="https://www.facebook.com/profile.php?id=100058188477258&sk=photos" target="_blank">
+                <img src="https://www.facebook.com/images/fb_icon_325x325.png" alt="Facebook" class="w-10 h-10">
+              </a>
+              <p class="text-lg mt-1 font-bold">Facebook</p>
+            </div>
+            <div class="mx-20 flex flex-col items-center">
+              <a href="https://www.youtube.com/channel/UCpNVpH6jDFAKYeyy0j_Z7NQ" target="_blank">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png?20220706172052"
+                  alt="YouTube" class="w-12 h-10">
+              </a>
+              <p class="text-lg mt-1 font-bold">YouTube</p>
+            </div>
+            <div class="mx-2 flex flex-col items-center">
+              <a href="https://github.com/kakelay" target="_blank">
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png"
+                  alt="GitHub" class="w-10 h-10">
+              </a>
+              <p class="text-lg mt-1 font-bold">GitHub</p>
+            </div>
+            <!-- Add more social media links/icons as needed -->
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+
+
+    <!-- Projects section -->
+    <section class="bg-white py-10">
+      <div class="container mx-auto">
+        <h2 class="text-2xl font-bold mb-4 text-center">My Projects</h2>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <!-- Add project cards with project details -->
+          <div class="bg-gray-200 p-4 rounded-lg shadow-md">
+            <h3 class="text-xl font-semibold">Project 1</h3>
+            <img
+              src="https://scontent.fpnh5-5.fna.fbcdn.net/v/t39.30808-6/370567086_703693364913606_2397925962208215234_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a2f6c7&_nc_ohc=R02srbZtjPgAX_U0RhC&_nc_ht=scontent.fpnh5-5.fna&oh=00_AfDUKiIfgPN4qjMhwa2kDKiXEKwf0317maBq5dfdV-yy5Q&oe=64F49B61"
+              alt="Project 1 Image" class="w-full h-auto mt-2">
+            <p class="text-gray-700 mt-2">
+              [Description of Project 1]
+            </p>
+          </div>
+          <div class="bg-gray-200 p-4 rounded-lg shadow-md">
+            <h3 class="text-xl font-semibold">Project 1</h3>
+            <img
+              src="https://scontent.fpnh5-4.fna.fbcdn.net/v/t39.30808-6/370626212_704731008143175_7420270604284996242_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=52f669&_nc_ohc=t8mU7so-a54AX92p9Lg&_nc_ht=scontent.fpnh5-4.fna&oh=00_AfAm8ggkOupjgvHI7N5Sj65Ql0IT_WomKYp-qNR2tKz50A&oe=64F3EA8D"
+              alt="Project 1 Image" class="w-full h-auto mt-2">
+            <p class="text-gray-700 mt-2">
+              [Description of Project 1]
+            </p>
+          </div>
+          <div class="bg-gray-200 p-4 rounded-lg shadow-md">
+            <h3 class="text-xl font-semibold">Project 1</h3>
+            <img
+              src="https://scontent.fpnh5-4.fna.fbcdn.net/v/t39.30808-6/370626212_704731008143175_7420270604284996242_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=52f669&_nc_ohc=t8mU7so-a54AX92p9Lg&_nc_ht=scontent.fpnh5-4.fna&oh=00_AfAm8ggkOupjgvHI7N5Sj65Ql0IT_WomKYp-qNR2tKz50A&oe=64F3EA8D"
+              alt="Project 1 Image" class="w-full h-auto mt-2">
+            <p class="text-gray-700 mt-2">
+              [Description of Project 1]
+            </p>
+          </div>
+          <div class="bg-gray-200 p-4 rounded-lg shadow-md">
+            <h3 class="text-xl font-semibold">Project 1</h3>
+            <img
+              src="https://scontent.fpnh5-4.fna.fbcdn.net/v/t39.30808-6/370626212_704731008143175_7420270604284996242_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=52f669&_nc_ohc=t8mU7so-a54AX92p9Lg&_nc_ht=scontent.fpnh5-4.fna&oh=00_AfAm8ggkOupjgvHI7N5Sj65Ql0IT_WomKYp-qNR2tKz50A&oe=64F3EA8D"
+              alt="Project 1 Image" class="w-full h-auto mt-2">
+            <p class="text-gray-700 mt-2">
+              [Description of Project 1]
+            </p>
+          </div>
+          <div class="bg-gray-200 p-4 rounded-lg shadow-md">
+            <h3 class="text-xl font-semibold">Project 1</h3>
+            <img
+              src="https://scontent.fpnh5-4.fna.fbcdn.net/v/t39.30808-6/352235550_1229877214333861_8875645456399175363_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=1b51e3&_nc_ohc=v_JdbZaz9tUAX-z527w&_nc_ht=scontent.fpnh5-4.fna&oh=00_AfCIV8GO0mXFPOcow5dRRQh1Yh-nxMV2u_4lavfrQ5ukHQ&oe=64F4BF22"
+              alt="Project 1 Image" class="w-full h-auto mt-2">
+            <p class="text-gray-700 mt-2">
+              [Description of Project 1]
+            </p>
+          </div>
+          <div class="bg-gray-200 p-4 rounded-lg shadow-md">
+            <h3 class="text-xl font-semibold">Project 1</h3>
+            <img
+              src="https://scontent.fpnh5-4.fna.fbcdn.net/v/t39.30808-6/370626212_704731008143175_7420270604284996242_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=52f669&_nc_ohc=t8mU7so-a54AX92p9Lg&_nc_ht=scontent.fpnh5-4.fna&oh=00_AfAm8ggkOupjgvHI7N5Sj65Ql0IT_WomKYp-qNR2tKz50A&oe=64F3EA8D"
+              alt="Project 1 Image" class="w-full h-auto mt-2">
+            <p class="text-gray-700 mt-2">
+              [Description of Project 1]
+            </p>
+          </div>
+          <div class="bg-gray-200 p-4 rounded-lg shadow-md">
+            <h3 class="text-xl font-semibold">Project 1</h3>
+            <img
+              src="https://scontent.fpnh5-2.fna.fbcdn.net/v/t39.30808-6/340635309_928484055083974_3930593728253721520_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=1b51e3&_nc_ohc=h_cOVUs8ojYAX-9uYjG&_nc_ht=scontent.fpnh5-2.fna&oh=00_AfDwXHF5jCh7CWTum2dKDeRQXD1DZWErEQuAMYT_yeMPGw&oe=64F3877F"
+              alt="Project 1 Image" class="w-full h-auto mt-2">
+            <p class="text-gray-700 mt-2">
+              [Description of Project 1]
+            </p>
+          </div>
+          <div class="bg-gray-200 p-4 rounded-lg shadow-md">
+            <h3 class="text-xl font-semibold">Project 1</h3>
+            <img
+              src="https://scontent.fpnh5-2.fna.fbcdn.net/v/t39.30808-6/365811595_693751155907827_6993819680295679773_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=1b51e3&_nc_ohc=GC6V3SUQZD4AX_06Jng&_nc_ht=scontent.fpnh5-2.fna&oh=00_AfCeqB8UJtsYHSulvpTe9enZkKv70R_0yz9KrUrhZs2DyQ&oe=64F4100B"
+              alt="Project 1 Image" class="w-full h-auto mt-2">
+            <p class="text-gray-700 mt-2">
+              [Description of Project 1]
+            </p>
+          </div>
+          <div class="bg-gray-200 p-4 rounded-lg shadow-md">
+            <h3 class="text-xl font-semibold">Project 1</h3>
+            <img
+              src="https://scontent.fpnh5-4.fna.fbcdn.net/v/t39.30808-6/370626212_704731008143175_7420270604284996242_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=52f669&_nc_ohc=t8mU7so-a54AX92p9Lg&_nc_ht=scontent.fpnh5-4.fna&oh=00_AfAm8ggkOupjgvHI7N5Sj65Ql0IT_WomKYp-qNR2tKz50A&oe=64F3EA8D"
+              alt="Project 1 Image" class="w-full h-auto mt-2">
+            <p class="text-gray-700 mt-2">
+              [Description of Project 1]
+            </p>
+          </div>
+          <div class="bg-gray-200 p-4 rounded-lg shadow-md">
+            <h3 class="text-xl font-semibold">Project 1</h3>
+            <img
+              src="https://scontent.fpnh5-4.fna.fbcdn.net/v/t39.30808-6/370626212_704731008143175_7420270604284996242_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=52f669&_nc_ohc=t8mU7so-a54AX92p9Lg&_nc_ht=scontent.fpnh5-4.fna&oh=00_AfAm8ggkOupjgvHI7N5Sj65Ql0IT_WomKYp-qNR2tKz50A&oe=64F3EA8D"
+              alt="Project 1 Image" class="w-full h-auto mt-2">
+            <p class="text-gray-700 mt-2">
+              [Description of Project 1]
+            </p>
+          </div>
+          <div class="bg-gray-200 p-4 rounded-lg shadow-md">
+            <h3 class="text-xl font-semibold">Project 1</h3>
+            <img
+              src="https://scontent.fpnh5-2.fna.fbcdn.net/v/t39.30808-6/371546525_705257704757172_8471433382776038338_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5614bc&_nc_ohc=QxjHinfDLykAX-KrWCw&_nc_ht=scontent.fpnh5-2.fna&oh=00_AfCkOpFMUQS2Nbypze-z0j33M_jF5v1izN85dkkmgSJwuA&oe=64F40DEA"
+              alt="Project 1 Image" class="w-full h-auto mt-2">
+            <p class="text-gray-700 mt-2">
+              [Description of Project 1]
+            </p>
+          </div>
+          <div class="bg-gray-200 p-4 rounded-lg shadow-md">
+            <h3 class="text-xl font-semibold">Project 1</h3>
+            <img
+              src="https://scontent.fpnh5-4.fna.fbcdn.net/v/t39.30808-6/370626212_704731008143175_7420270604284996242_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=52f669&_nc_ohc=t8mU7so-a54AX92p9Lg&_nc_ht=scontent.fpnh5-4.fna&oh=00_AfAm8ggkOupjgvHI7N5Sj65Ql0IT_WomKYp-qNR2tKz50A&oe=64F3EA8D"
+              alt="Project 1 Image" class="w-full h-auto mt-2">
+            <p class="text-gray-700 mt-2">
+              [Description of Project 1]
+            </p>
+          </div>
+
+          <!-- Add more project cards as needed -->
+        </div>
+      </div>
+    </section>
+    <!-- About section -->
+    <!-- <section class="py-10 bg-image"> -->
+    <section class="about-section text-2x  font-bold mb- text-center">
+      <div class="container mx-auto">
+        <h2 class="text-2xl font-bold mb-4">About Me</h2>
+        <p class="text-gray-700 font-bold mb-4" align="left">
+          Name: Kak Ekay
+        </p>
+        <p class="text-gray-700 font-bold mb-4" align="left">
+          Age: 21
+        </p>
+        <p class="text-gray-700 font-bold mb-4" align="left">
+          Name: Kak Ekay
+        </p>
+        <p class="text-gray-700 font-bold mb-4" align="left">
+          Education: Studying at NORTON University
+        </p>
+        <p class="text-gray-700 font-bold mb-4" align="left">
+          Subject: Software Development
+        </p>
+        <p class="text-gray-700 font-bold mb-4" align="left">
+          Location: Phnom Penh City
+        </p>
+        <p class="text-gray-700 font-bold mb-4" align="left">
+          Phone Number: 010600261
+        </p>
+
+      </div>
+    </section>
+
+
+
+    <!-- Footer -->
+
+
+
+    <section class="py-10 bg-gray-300">
+      <div class="container mx-auto text-center">
+
+        <h2 class="text-2xl font-bold text-black">Contact Me</h2>
+        <p class="text-black mt-4  text-2xl">
+          Phone Number: 010600261
+        </p>
+        <p class="text-blackwhite  text-2xl">
+          Telegram: @elaydev
+        </p>
+        <p class="text-black text-2xl ">
+          Gmail: kakelay18052002@gmail.com
+        </p>
+      </div>
+
+      <div class="map-container mt-10">
+        <div class="zoom-buttons">
+          <button @click="zoomIn" class="zoom-button">Zoom In</button>
+          <button @click="zoomOut" class="zoom-button">Zoom Out</button>
+        </div>
+        <iframe ref="mapIframe" src="https://www.google.com/maps/embed?pb=..." width="100%" height="0" style="border:0;"
+          allowfullscreen="" loading="lazy">
+        </iframe>
+      </div>
+       <footer class="bg-gray-300 py-4 text-center">
+       <h2 class="text-2xl font-bold mb-4 text-center">Develop by KakElay Company </h2>
+          <div class="flex justify-center">
+            <img
+              src="https://scontent.fpnh5-2.fna.fbcdn.net/v/t39.30808-6/373297621_705830021366607_2973753929437902708_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5614bc&_nc_ohc=jFLaIsrW4YAAX8Sbthv&_nc_ht=scontent.fpnh5-2.fna&oh=00_AfC3z-mgFTKMemC4P_b-lNy5yIeibgo1Ic_MkPAjLfuXmA&oe=64F41F23"
+              alt="Profile Image" class="w-32 h-32 rounded-full">
+          </div>
+         
+      </footer>
+
+    </section>
+
+
+
+
+    <!-- Footer -->
+
+
+
+  </div>
+</template>
+
+
+
+
+
+/// sytle  css customer
+
+<script>
+export default {
+  name: 'SelfIntroduction',
+  name: 'SelfIntroduction',
+  methods: {
+    zoomIn() {
+      const iframe = this.$refs.mapIframe;
+      iframe.contentWindow.postMessage('{"event":"command","func":"zoomIn","args":[]}', '*');
+    },
+    zoomOut() {
+      const iframe = this.$refs.mapIframe;
+      iframe.contentWindow.postMessage('{"event":"command","func":"zoomOut","args":[]}', '*');
+    }
+  },
+
+  mounted() {
+    // Load Google Maps JavaScript API
+    const script = document.createElement("script");
+    script.src = `https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places`;
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      this.initMap();
+    };
+  },
+
+  methods: {
+    initMap() {
+      // Initialize the map and add any additional functionality here
+      // You can also add code to center the map to your location
+    },
+
+    // ... your other methods ...
+  },
+};
+</script>
+
+<style scoped>
+.bg-image {
+  background-image: url('https://www.trio.dev/hubfs/Imported_Blog_Media/types%20of%20software%20development.webp');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+.about-section {
+  background-color: #6b636325;
+  /* Replace with the desired background color */
+  padding: 20px;
+  /* Add some padding to create space between content and background */
+}
+
+
+.map-container {
+  position: relative;
+  width: 100%;
+  padding-bottom: 56.25%;
+  /* 16:9 aspect ratio */
+}
+
+.map-container iframe {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0%;
+}
+footer {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
+
